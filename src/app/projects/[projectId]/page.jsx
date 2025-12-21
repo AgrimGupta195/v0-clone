6216de28e879a1c576e38fb4dbef3b12b7a25025
Agrim Gupta <agrimgupt195@gmail.com>
2025-12-21 21:59:51 +0530

    fix compile issue

--- src/app/projects/[projectId]/page.jsx
+++ src/app/projects/[projectId]/page.jsx
@@ -1,17 +1,12 @@
-import db from '@/lib/db';
-import { getMessages } from '@/modules/messages/actions';
-import { getProjectById } from '@/modules/projects/actions';
 import ProjectView from '@/modules/projects/components/project-view';
-import React, { Suspense } from 'react'
+import React from 'react';
 
-const Page = async({params}) => {
-    const {projectId} = await params;
+export const runtime = "nodejs";
 
+const Page = async ({ params }) => {
+  const { projectId } = params;
 
-  return (
-      <ProjectView projectId={projectId} />
- 
-  )
-}
+  return <ProjectView projectId={projectId} />;
+};
 
-export default Page
+export default Page;
