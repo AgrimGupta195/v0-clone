b80cbab722d820ea0b6c88396cbba3532b3fb3f3
Agrim Gupta <agrimgupt195@gmail.com>
2025-12-21 20:50:20 +0530

    fix name

--- src/app/(root)/layout.jsx
+++ src/app/(root)/layout.jsx
@@ -1,5 +1,5 @@
 import { onBoardUser } from "@/modules/auth/actions";
-import { Navbar } from "@/modules/home/components/navbar";
+import { Navbar } from "@/modules/home/components/Navbar";
 
 import React from "react";
 
