7450cae8ab41d2000a177c3c697c6a2fa062398a
Agrim Gupta <agrimgupt195@gmail.com>
2025-12-21 21:48:14 +0530

    fix db conn

--- src/lib/db.js
+++ src/lib/db.js
@@ -4,18 +4,24 @@ import "server-only";
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
+const globalForPrisma = globalThis;
+
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 });
 
-const globalAny = globalThis;
-const db = globalAny.__db || new PrismaClient({
-  adapter,
-  log: process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : ['error'],
-});
+const db =
+  globalForPrisma.__prisma ??
+  new PrismaClient({
+    adapter,
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "info", "warn", "error"]
+        : ["error"],
+  });
 
 if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.__db = db;
+  globalForPrisma.__prisma = db;
 }
 
 export default db;
