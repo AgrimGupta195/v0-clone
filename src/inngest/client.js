665243335a1b02979bc9d721041a0b4d94b85502
Agrim Gupta <agrimgupt195@gmail.com>
2025-12-26 23:56:04 +0530

    name change

--- src/inngest/client.js
+++ src/inngest/client.js
@@ -1,4 +1,7 @@
 import { Inngest } from "inngest";
 
-
-export const inngest = new Inngest({ id: "v0-clone" });
+export const inngest = new Inngest({ 
+  id: "v0-clone",
+  // Add your Inngest signing key from environment variables if using Inngest Cloud
+  signingKey: process.env.INNGEST_SIGNING_KEY,
+});
